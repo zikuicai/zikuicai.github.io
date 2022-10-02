99db8a995bfe75921bfa3d2dfd814831c2f31127
zikuicai <zikuicai@gmail.com>
2022-10-02 13:52:22 -0700

    update

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-
-// emp Zikui Cai 
+// add underline to Zikui Cai 
 let author_tags = document.getElementsByTagName("author");
 for (let i = 0; i < author_tags.length; i++) {
     author_tags[i].innerHTML = author_tags[i].textContent.replace('Zikui Cai', '<u>Zikui Cai</u>')
 }
-// window.alert(author_tags[0].textContent)
